0586997e76578cd91b2f77e1bdac5a27343cd2f1
Abic26 <abicsupa@gmail.com>
2023-08-07 04:09:51 -0500

    vercel

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script>
-import NavBar from './components/NavBar.vue';
+import NavBar from './components/Navbar.vue';
 import Main from './components/Main.vue';
 import bg from './components/Background.vue';
 import Modal_Login from './components/Modal_Login.vue';
 // import prueba from './components/prueba.vue';
 // import Index_Login from './components/Index_Login.vue';
-
 
 
 
